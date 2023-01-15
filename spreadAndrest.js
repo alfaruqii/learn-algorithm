e7aa4573c9836d882e42bad575170b0e1684a4bb
alfaruqii <bumbblechover@gmail.com>
2023-01-16 02:49:10 +0700

    change: add an index console on spreadAndrest.js

--- spreadAndrest.js
+++ spreadAndrest.js
@@ -4,8 +4,9 @@ let profile = [18, "math", true, "dude", other]
 function myFunc(personOne, age, skill, isStudent, gender, dll) {
   console.log(personOne)
   console.log([age, skill, isStudent, gender])
-  dll.forEach((item) => {
+  dll.forEach((item, index) => {
     console.log(item)
+    console.log(index)
   })
 }
 
