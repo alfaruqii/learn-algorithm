ed7c119e30f7e5e6c772c4d57b2da2bfaf08fd47
alfaruqii <bumbblechover@gmail.com>
2023-01-16 03:08:44 +0700

    change: remove an index console on spreadAndrest.js

--- spreadAndrest.js
+++ spreadAndrest.js
@@ -4,9 +4,8 @@ let profile = [18, "math", true, "dude", other]
 function myFunc(personOne, age, skill, isStudent, gender, dll) {
   console.log(personOne)
   console.log([age, skill, isStudent, gender])
-  dll.forEach((item, index) => {
+  dll.forEach((item) => {
     console.log(item)
-    console.log(index)
   })
 }
 
